5613ed6adbe73d9126fa8c1a5f7d16066b36cfb0
Logan Ward <ward.logan.t@gmail.com>
2019-12-11 15:55:23 +0000

    Fixed how memory size is computed

--- qcengine/programs/nwchem/runner.py
+++ qcengine/programs/nwchem/runner.py
@@ -121,8 +121,8 @@ class NWChemHarness(ProgramHarness):
         # for nwchem, [GiB] --> [B]
         # someday, replace with this: opts['memory'] = str(int(config.memory * (1024**3) / 1e6)) + ' mb'
         memory_size = int(config.memory * (1024 ** 3))
-        if config.use_mpiexec:  # It is the memory per MPI rank
-            memory_size //= config.nnodes * config.ncores
+        if config.use_mpirun:  # Divide memory across MPI ranks per node
+            memory_size //= config.ncores
         opts["memory"] = memory_size
 
         # Handle molecule
